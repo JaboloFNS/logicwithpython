899b96c9ab17ba65e12958d4da623834160f4a38
Fabiano Novais <fabiano.novais.99@gmail.com>
2025-01-05 13:42:36 -0300

    Exemplos de aula sobre o laço de repetição While

---
+++ testeswhile.py
@@ -0,0 +1,15 @@
+contador = 1
+
+while contador <=10:
+    print (contador)
+    contador += 1
+
+print (f"O valor final da variável é: {contador}")
+
+contador_d = 10
+
+while contador_d >= 1:
+    print (contador_d)
+    contador_d -= 1
+
+print (f"O valor final da variável é: {contador_d}")
