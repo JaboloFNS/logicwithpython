dd1473379249a5c2e6affb3c38195075f8d764b2
Fabiano Novais <fabiano.novais.99@gmail.com>
2025-01-05 14:14:04 -0300

    Exercícios sobre o laço de repetição While e suas aplicações mais simples para outputs numéricos

--- testeswhile.py
+++ testeswhile.py
@@ -1,3 +1,5 @@
+# TESTE 1 - OUTPUT NUMÉRICO EM ORDEM CRESCENTE
+
 contador = 1
 
 while contador <=10:
@@ -6,6 +8,8 @@ while contador <=10:
 
 print (f"O valor final da variável é: {contador}")
 
+# TESTE 2 - OUTPUT NUMÉRICO EM ORDEM DECRESCENTE
+
 contador_d = 10
 
 while contador_d >= 1:
@@ -13,3 +17,36 @@ while contador_d >= 1:
     contador_d -= 1
 
 print (f"O valor final da variável é: {contador_d}")
+
+# TESTE 3 - DADA A CONDIÇÃO, CONFIRA SE UM NUMERO É PAR OU ÍMPAR
+
+num = 5
+while num > 0:
+    if num % 2 == 0:
+        print (f"{num} é par")
+    else:
+        print (f"{num} é ímpar")
+    num -= 1
+print(f"O valor final de num é: {num}")
+
+#TESTE 4 - OUTPUT DE SEQUÊNCIA NUMÉRICA EM LOOPING INFINITO COM UMA CONDICIONAL QUE PARA ESSE LOOPING QUANDO VERIFICADA
+
+num = 1
+while num > 0:
+    print (num)
+    num += 1
+
+    if num == 100:
+        break
+
+# TESTE 5 - WHILES ANINHADOS COM OUTPUT NUMÉRICO
+
+i = 1
+
+while i <= 3:
+    j = 1
+    while j <= 2:
+        print(f"i={i}, j={j}")
+        j += 1
+    i += 1
+
