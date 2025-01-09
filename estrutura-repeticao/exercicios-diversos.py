78d95b68586d7e195ab57d3d529a0f2f4bcc146f
Fabiano Novais <fabiano.novais.99@gmail.com>
2025-01-08 22:18:04 -0300

    Exercícios de Estrutura de Repeticao

---
+++ estrutura-repeticao/exercicios-diversos.py
@@ -0,0 +1,22 @@
+# 1) Desenvolver um algoritmo que efetue a soma de todos os números ímpares que são múltiplos de
+# três e que se encontram no conjunto dos números de 1 até 500.
+
+for i in range(1,500,1):
+    if i % 3 == 0:
+        print(i)
+
+# 2) Desenvolver um algoritmo que leia a altura de 15 pessoas. Este programa deverá calcular e
+# mostrar :
+# a. A menor altura do grupo;
+# b. A maior altura do grupo;
+
+alturas = []
+
+for i in range(5):
+    altura = int(input(f"Digite a altura da {i+1}ª pessoa, em centímetros:"))
+    alturas.append(altura)
+
+maior_altura = max(alturas)
+menor_altura = min(alturas)
+
+print(f"A maior altura é de {maior_altura}cm e a menor altura é de {menor_altura}cm")
